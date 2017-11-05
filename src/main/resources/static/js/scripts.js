2b06268e1a96c91d66444708942faad71489bb8f
Randle McMurphy <jmarton2000@gmail.com>
2017-11-05 17:29:06 +0200

    Clean-up JavaScript

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,49 +1,5 @@
-/**
- * 
- */
-
-/* checkout.html */
-function checkOrderBillingAddress() {
-	if($("#theSameAsOrderShipping").is(":checked")) {
-		$(".orderBilling").prop("disabled", true);
-	} else {
-		$(".orderBilling").prop("disabled", false);
-	}
-}
-
-function checkPasswordMatch() {
-	var password = $("#txtNewPassword").val();
-	var confirmPassword = $("#txtConfirmPassword").val();
-	
-	if(password == "" && confirmPassword =="") {
-		$("#checkPasswordMatch").html("");
-		$("#updateUserInfoButton").prop('disabled', false);
-		$("#newPasswords").prop('class', 'form-group');
-	} else {
-		if(password != confirmPassword) {
-			$("#checkPasswordMatch").html("Passwords do not match!");
-			$("#updateUserInfoButton").prop('disabled', true);
-			$("#newPasswords").prop('class', 'form-group has-error');
-		} else {
-			$("#checkPasswordMatch").html("Passwords match");
-			$("#updateUserInfoButton").prop('disabled', false);
-			$("#newPasswords").prop('class', 'form-group');
-		}
-	}
-}
-
-/* myAccount.html, shoppingCart.html, checkout.html */
-$(document).ready(function(){
-	$(".cartItemQty").on('change', function(){
-		var id=this.id;
-		
-		$('#update-item-'+id).css('display', 'inline-block');
-	});
-	$("#theSameAsOrderShipping").on('click', checkOrderBillingAddress);
-	$("#txtConfirmPassword").keyup(checkPasswordMatch);
-	$("#txtNewPassword").keyup(checkPasswordMatch);
-	
-	//$("#addMore").on('click', addMore);
+/* index.html, shoppingCart.html, checkout.html */
+$(document).ready(function() {
 	var max_rows      	= 50;
 	var wrapper         = $("#myTBody");
 	var addRowButton    = $("#addRowButton");
@@ -51,7 +7,8 @@ $(document).ready(function(){
 
 	var rowList = document.getElementsByClassName(rowClassName);
 	var rowCount = rowList.length;	// number of rows
-	
+
+	// ****************** Delete One Row ******************
 	// hide 'Delete' button if there's only one row
 	if (rowCount == 1) 
 		$('.deleteRow').css('display', 'none');
@@ -67,6 +24,7 @@ $(document).ready(function(){
     		$('.deleteRow').css('display', 'none');
     });
 	
+	// ****************** Add One Row ******************
     $(addRowButton).click(function(e) {
     	e.preventDefault();
 		if (rowCount < max_rows){
@@ -80,6 +38,7 @@ $(document).ready(function(){
 		}
 	  });
     
+	// ****************** Resizing window ******************
 	toggle();
     window.onresize = function() { 
     	toggle();
@@ -93,6 +52,7 @@ $(document).ready(function(){
         }    
     }
     
+	// ****************** Delete All Rows ******************
 	$('#deleteAll').click(function(e) {
 		e.preventDefault();
 		/*<![CDATA[*/
@@ -118,7 +78,6 @@ $(document).ready(function(){
 			}
 		});
 	});
-    
 	  
 });
 
@@ -266,106 +225,3 @@ function appendRow(wrapper) {
 	document.getElementById("myTBody").appendChild(newRow);
 	//document.getElementById("myTBody").appendChild(document.createElement("br"));	// Append a line break
 }
-
-/* ****************************************************
- * 
- * /admin scripts 
- * 
- * */
-
-$(document).ready(function() {
-	$('.delete-book').on('click', function (){
-		/*<![CDATA[*/
-	    var path = /*[[@{/}]]*/'/admin/remove';
-	    /*]]>*/
-		
-		var id=$(this).attr('id');
-		
-		console.log(path);
-		
-		bootbox.confirm({
-			message: "Are you sure to remove this book? It can't be undone.",
-			buttons: {
-				cancel: {
-					label:'<i class="fa fa-times"></i> Cancel'
-				},
-				confirm: {
-					label:'<i class="fa fa-check"></i> Confirm'
-				}
-			},
-			callback: function(confirmed) {
-				if(confirmed) {
-					$.post(path, {'id':id}, function(res) {
-						location.reload();
-					});
-				}
-			}
-		});
-	});
-	
-	
-	
-//	$('.checkboxBook').click(function () {
-//        var id = $(this).attr('id');
-//        if(this.checked){
-//            bookIdList.push(id);
-//        }
-//        else {
-//            bookIdList.splice(bookIdList.indexOf(id), 1);
-//        }
-//    })
-	
-	$('#deleteSelected').click(function() {
-		var idList= $('.checkboxBook');
-		var bookIdList=[];
-		for (var i = 0; i < idList.length; i++) {
-			if(idList[i].checked==true) {
-				bookIdList.push(idList[i]['id'])
-			}
-		}
-		
-		console.log(bookIdList);
-		
-		/*<![CDATA[*/
-	    var path = /*[[@{/}]]*/'/admin/removeList';
-	    /*]]>*/
-	    
-	    bootbox.confirm({
-			message: "Are you sure to remove all selected books? It can't be undone.",
-			buttons: {
-				cancel: {
-					label:'<i class="fa fa-times"></i> Cancel'
-				},
-				confirm: {
-					label:'<i class="fa fa-check"></i> Confirm'
-				}
-			},
-			callback: function(confirmed) {
-				if(confirmed) {
-					$.ajax({
-						type: 'POST',
-						url: path,
-						data: JSON.stringify(bookIdList),
-						contentType: "application/json",
-						success: function(res) {
-							console.log(res); 
-							location.reload()
-							},
-						error: function(res){
-							console.log(res); 
-							location.reload();
-							}
-					});
-				}
-			}
-		});
-	});
-	
-	$("#selectAllBooks").click(function() {
-		if($(this).prop("checked")==true) {
-			$(".checkboxBook").prop("checked",true);
-		} else if ($(this).prop("checked")==false) {
-			$(".checkboxBook").prop("checked",false);
-		}
-	})
-});
